73a737293a274772ee1d3801ada95a05b6ab557c
kovarny <sivochkin@gmail.com>
2022-10-12 12:31:29 +1100

    removed dependency on axios

--- functions/src/fetch.ts
+++ functions/src/fetch.ts
@@ -1,6 +1,91 @@
+// import axios from "axios";
 import * as functions from "firebase-functions";
 import * as sanctions from "sanctions";
 import { saveList } from "./common";
+// import fetch from 'node-fetch';
+// const XLSX = require("xlsx");
+// import { dfat_gov_au__consolidated_list1 } from "./dfat_gov_au__consolidated_list";
+const cors = require('cors')({ origin: true });
+
+export const get_dfat_gov_au__consolidated_list = functions.runWith({timeoutSeconds:500}).https
+.onRequest(async (request, res) => {
+  cors(request, res,async () => {
+
+    console.log('load list...');
+    const result=await sanctions.dfat_gov_au__consolidated_list();
+    console.log('saveList...');
+    await saveList(await sanctions.dfat_gov_au__consolidated_list(), 
+      'dfat_gov_au__consolidated_list', "Name of Individual or Entity");
+      res.send(result);
+
+//   console.log('fetch list...');
+//   //const res=await dfat_gov_au__consolidated_list1();
+//   // const resp = 
+//   const response = await fetch('https://www.dfat.gov.au/sites/default/files/regulation8_consolidated.xls');
+// // const body = await response.buffer();//.text();
+// // console.log(`${body}`);
+// // console.log('axios...');
+//   // await axios({
+//   //   method: "GET",
+//   //   url: "https://www.dfat.gov.au/sites/default/files/regulation8_consolidated.xls",
+//   //   responseType: "arraybuffer",
+//   // }).then(response => {
+//   //   console.log(response.data);
+//   //   return res.status(200).json({
+//   //     message: response.data.ip
+//   //   })
+//   // })
+//   // .catch(err => {
+//   //   return res.status(500).json({
+//   //     error: err
+//   //   })
+//   // })
+//   const buffers: any = [];
+//   console.log("pushing to buffer");
+//     buffers.push(await response.buffer());
+//     const buffer = Buffer.concat(buffers);
+//     const workbook = XLSX.read(buffer);
+//     const sheetName = workbook.SheetNames[0];
+//     console.log("sheetName ", sheetName);
+
+//     // CONVERT SHEET TO JSON
+//   const auIndividuals = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+//   console.log("au Individuals count ", auIndividuals.length);
+
+//   let result = [];
+//   for (const person of auIndividuals as any) {
+//     // console.log(
+//     //   `loop # ${++count} -- looping over ${person["Name of Individual or Entity"]}`
+//     // );
+
+//     let p: any = {};
+
+//     p["Reference"] = person["Reference"] || "";
+//     p["Name of Individual or Entity"] =
+//       person["Name of Individual or Entity"] || "";
+//     p["Type"] = person["Type"] || "";
+//     p["Name Type"] = person["Name Type"] || "";
+//     p["Date of Birth"] = person["Date of Birth"] || "";
+//     p["Place of Birth"] = person["Place of Birth"] || "";
+//     p["Citizenship"] = person["Citizenship"] || "";
+//     p["Address"] = person["Address"] || "";
+//     p["Additional Information"] = person["Additional Information"] || "";
+//     p["Listing Information"] = person["Listing Information"] || "";
+//     p["Committees"] = person["Committees"] || "";
+//     p["Control Date"] = person["Control Date"] || "";
+
+//     const docId = person["Name of Individual or Entity"];
+
+//     console.log("docId ", docId);
+
+//     result.push(p);
+//   }
+
+  
+//   res.send(result);
+//   // res.send('done');
+  })
+});
 
 
 export const dfat_gov_au__consolidated_list = functions.pubsub
@@ -35,12 +120,12 @@ export const ec_europa_eu__sanctions_list = functions.pubsub
     'ec_europa_eu__sanctions_list', "Name of Individual or Entity");
 });
 
-export const government_nl__dnslt = functions.pubsub
+export const government_nl__dutch_national_sanctions_list = functions.pubsub
 .schedule("5 11 * * *")
 .timeZone("Australia/Sydney")
 .onRun(async () => {
-    await saveList(await sanctions.government_nl__dnslt(), 
-    'government_nl__dnslt', "Name of Individual or Entity");
+    await saveList(await sanctions.government_nl__dutch_national_sanctions_list(), 
+    'government_nl__dutch_national_sanctions_list', "Name of Individual or Entity");
 });
 
 // //Have to add in index.js
@@ -71,12 +156,12 @@ export const un_org__consolidated_individuals = functions.pubsub
 // });
 
 
-export const treasury_gov__sdnl = functions.pubsub
+export const treasury_gov__nonsdnl = functions.pubsub
 .schedule("5 11 * * *")
 .timeZone("Australia/Sydney")
 .onRun(async () => {
-    await saveList(await sanctions.treasury_gov__sdnl(), 
-    'treasury_gov__sdnl', "Name of Individual or Entity");
+    await saveList(await sanctions.treasury_gov__nonsdnl(), 
+    'treasury_gov__nonsdnl', "Name of Individual or Entity");
 });
 
 
@@ -129,7 +214,7 @@ export const occ_gov__enforcement_actions = functions.pubsub
 .schedule("5 11 * * *")
 .timeZone("Australia/Sydney")
 .onRun(async () => {
-    await saveList(await sanctions.occ_gov_enforcement_actions(), 
+    await saveList(await sanctions.occ_gov__enforcement_actions(), 
     'occ_gov__enforcement_actions', "Name of Individual or Entity");
 });
 
