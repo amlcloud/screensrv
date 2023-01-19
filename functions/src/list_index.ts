256528403ab1e1b15ac7839e70c3737c354bbd37
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-01-20 01:32:54 +1100

    updated indexing function to support status report

--- functions/src/list_index.ts
+++ functions/src/list_index.ts
@@ -1,5 +1,5 @@
 // import axios from "axios";
-import { FieldValue } from "firebase-admin/firestore";
+import { DocumentReference, FieldValue } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
 import { db } from ".";
 import { safeString } from "./common";
@@ -64,79 +64,89 @@ export const index_list2 = functions.runWith({timeoutSeconds:540}).https
 .onRequest(async (request, res) => {
   cors(request, res,async () => {
     const listId:string=request.query.list as string;
+    const deleteRequest = request.query.delete as string;
+    const statusColRef = db.collection('indexStatus');
+    await deleteLargeColByQuery(statusColRef.where('listId', '==', listId))
     await deleteLargeColByQuery(db.collection('index').where('listId', '==', listId));
-    let counter = 0;
-    let batch = db.batch();
-    const reference = await db.collection('list').doc(listId);
-    const indexConfigs = await reference.collection('indexConfigs').get();
-    const items = await reference.collection('item').get();
-    for (let indexConfig of indexConfigs.docs) {
-      let entityIndexFields = await indexConfig.ref.collection('entityIndexFields').get();
-      var valid = true;
-      for (let entityIndexField of entityIndexFields.docs) {
-        if (!entityIndexField.data().valid) {
-          valid = false;
-          break;
+    if (deleteRequest !== 'true') {
+      let counter = 0;
+      let indexMap = new Map<DocumentReference, boolean>();
+      let batch = db.batch();
+      const reference = await db.collection('list').doc(listId);
+      const indexConfigs = await reference.collection('indexConfigs').get();
+      const items = await reference.collection('item').get();
+      let statusRef = await statusColRef.add({listId: listId, count: 0, total: items.size});
+      for (let indexConfig of indexConfigs.docs) {
+        let entityIndexFields = await indexConfig.ref.collection('entityIndexFields').get();
+        var valid = true;
+        for (let entityIndexField of entityIndexFields.docs) {
+          if (!entityIndexField.data().valid) {
+            valid = false;
+            break;
+          }
         }
-      }
-      if (valid && entityIndexFields.docs.length > 0) {
-        let type = indexConfig.data().type;
-        for (let item of items.docs) {
-          if (type === 'Single field') {
-            let value = item.data()[entityIndexFields.docs[0].data().value];
-            if (!Array.isArray(value)) {
-              addToBatch(batch, listId, item.ref, value);
-              counter++;
-              if (counter > 490) {
-                await batch.commit();
-                batch = db.batch();
-                counter = 0;
-              }
-            }
-          } else if (type === 'Multiple fields') {
-            var containsArray = false;
-            var name = '';
-            for (let entityIndexField of entityIndexFields.docs) {
-              let value = item.data()[entityIndexField.data().value];
-              if (Array.isArray(value)) {
-                containsArray = true;
-                break;
-              }
-              name += (name.length > 0 ? ' ' : '') + value;
-            }
-            if (!containsArray) {
-              addToBatch(batch, listId, item.ref, name);
-              counter++;
-              if (counter > 490) {
-                await batch.commit();
-                batch = db.batch();
-                counter = 0;
-              }
-            }
-          } else if (type === 'Array of values') {
-            let values = item.data()[entityIndexFields.docs[0].data().value];
-            if (Array.isArray(values)) {
-              for (let value of values) {
-                addToBatch(batch, listId, item.ref, value);
+        if (valid && entityIndexFields.docs.length > 0) {
+          let type = indexConfig.data().type;
+          for (let item of items.docs) {
+            if (type === 'Single field') {
+              let value = item.data()[entityIndexFields.docs[0].data().value];
+              if (!Array.isArray(value)) {
+                addToBatch(batch, listId, item.ref, value, statusRef, indexMap);
                 counter++;
+                indexMap.set(item.ref, true);
                 if (counter > 490) {
                   await batch.commit();
                   batch = db.batch();
                   counter = 0;
                 }
               }
+            } else if (type === 'Multiple fields') {
+              var containsArray = false;
+              var name = '';
+              for (let entityIndexField of entityIndexFields.docs) {
+                let value = item.data()[entityIndexField.data().value];
+                if (Array.isArray(value)) {
+                  containsArray = true;
+                  break;
+                }
+                name += (name.length > 0 ? ' ' : '') + value;
+              }
+              if (!containsArray) {
+                addToBatch(batch, listId, item.ref, name, statusRef, indexMap);
+                counter++;
+                indexMap.set(item.ref, true);
+                if (counter > 490) {
+                  await batch.commit();
+                  batch = db.batch();
+                  counter = 0;
+                }
+              }
+            } else if (type === 'Array of values') {
+              let values = item.data()[entityIndexFields.docs[0].data().value];
+              if (Array.isArray(values)) {
+                for (let value of values) {
+                  addToBatch(batch, listId, item.ref, value, statusRef, indexMap);
+                  counter++;
+                  indexMap.set(item.ref, true);
+                  if (counter > 490) {
+                    await batch.commit();
+                    batch = db.batch();
+                    counter = 0;
+                  }
+                }
+              }
             }
           }
         }
       }
+      await batch.commit();
     }
-    await batch.commit();
     res.send(`indexed list ${request.query.list}`);
   })
 });
 
 
-function addToBatch(batch:any, listId:any, ref:any, name:string)
+function addToBatch(batch:any, listId:any, ref:any, name:string, statusRef:DocumentReference, indexMap:Map<DocumentReference, boolean>)
 {
   batch.set(
     db
@@ -149,6 +159,7 @@ function addToBatch(batch:any, listId:any, ref:any, name:string)
         't': FieldValue.serverTimestamp(),
         ...gramCounterBool(name, 2),
       });
+  statusRef.update({count: [...indexMap.keys()].length});
 }
 
 export async function deleteLargeColByQuery(query: Query) {
