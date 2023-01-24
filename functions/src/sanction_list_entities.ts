cd15b9ab68ddf225af3aa2ac2cec37b703fc4954
Oleksii Pavliuk <capetruha@gmail.com>
2023-01-24 12:44:23 +1100

    Updated GetSanctionListEntities

--- functions/src/sanction_list_entities.ts
+++ functions/src/sanction_list_entities.ts
@@ -4,14 +4,27 @@ import { db } from "./index";
 //returns the list of items (in JSON) of the sanction list.
 export const GetSanctionListEntities = functions.https.onRequest(
   async (req, res) => {
-    const listDoc = await db.collection("list").doc(req.url.slice(1)).get();
 
-    if (!listDoc.exists) {
-      res.status(404).send("List not found");
-      return;
+    // Checking if request method was POST or GET and if item was provided
+    if(req.method == 'GET'){
+      var item: any = req.query.item
+    }else if(req.method == 'POST'){
+      var item = req.body.item
+    }
+    if(!item){
+      res.status(400).send('"item" was not provided')
+      return
     }
 
-    const responce = await db.collection("list").doc(req.url.slice(1)).collection('item').get()
-    res.status(200).send(responce.docs.map((d) => d.data()));
+    //Checking if user provided correct item
+    const listDoc = await db.collection("list").doc(item).get()
+    if (!listDoc.exists) {
+      res.status(404).send("Item not found")
+      return
+    }
+
+    //Sending response
+    const responce = await db.collection("list").doc(item).collection('item').get()
+    res.status(200).send(responce.docs.map((d) => d.data()))
   }
 );
