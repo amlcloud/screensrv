8b4a5e715e1207cce38353f98ddd3eddd9cad5bf
Oleksii Pavliuk <capetruha@gmail.com>
2023-01-25 12:40:44 +1100

    List is now named 'list' not 'item'

--- functions/src/sanction_list_entities.ts
+++ functions/src/sanction_list_entities.ts
@@ -7,24 +7,24 @@ export const GetSanctionListEntities = functions.https.onRequest(
 
     // Checking if request method was POST or GET and if item was provided
     if(req.method == 'GET'){
-      var item: any = req.query.item
+      var list: any = req.query.list
     }else if(req.method == 'POST'){
-      var item = req.body.item
+      var list = req.body.list
     }
-    if(!item){
-      res.status(400).send('"item" was not provided')
+    if(!list){
+      res.status(400).send('Parameter "list" - name of list was not provided')
       return
     }
 
     //Checking if user provided correct item
-    const listDoc = await db.collection("list").doc(item).get()
+    const listDoc = await db.collection("list").doc(list).get()
     if (!listDoc.exists) {
       res.status(404).send("Item not found")
       return
     }
 
     //Sending response
-    const responce = await db.collection("list").doc(item).collection('item').get()
+    const responce = await db.collection("list").doc(list).collection('item').get()
     res.status(200).send(responce.docs.map((d) => d.data()))
   }
 );
