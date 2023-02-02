ed639a0831762485d8460bb7765975fd0c3f7852
Tung Lok Wan <tunglokw@student.unimelb.edu.au>
2023-02-03 02:00:04 +1100

    extracted field meta data and stored in a separate collection

--- functions/src/common.ts
+++ functions/src/common.ts
@@ -3,11 +3,18 @@ import {
 	DocumentReference,
 	FieldValue,
 } from "firebase-admin/firestore";
+import {
+	CollectionReference,
+	DocumentReference,
+	FieldValue,
+} from "firebase-admin/firestore";
 import { db } from "./index";
 import { createHash } from "node:crypto";
 
 const FIRESTORE_WRITE_BATCH_SIZE = 450;
 
+export function safeString(unsafe: string): string {
+	return unsafe.replace(/\//gi, "_");
 export function safeString(unsafe: string): string {
 	return unsafe.replace(/\//gi, "_");
 }
@@ -26,22 +33,12 @@ export async function saveFields(jsonArray: any[], listId: string) {
 			}
 		}
 	}
-	let batch = db.batch();
-	let counter = 0;
 	let colRef: CollectionReference = db
 		.collection("list")
 		.doc(listId)
 		.collection("fields");
-	let fields = await colRef.listDocuments();
-	for (let i = 0; i < fields.length; i++) {
-		batch.delete(fields[i]);
-		counter++;
-		if (counter > FIRESTORE_WRITE_BATCH_SIZE) {
-			await batch.commit();
-			batch = db.batch();
-			counter = 0;
-		}
-	}
+	let counter = 0;
+	let batch = db.batch();
 	for (let key in dict) {
 		batch.set(colRef.doc(key), dict[key]);
 		counter++;
@@ -55,6 +52,25 @@ export async function saveFields(jsonArray: any[], listId: string) {
 }
 
 // Saving list
+export async function saveList(
+	jsonArray: any[],
+	listId: string,
+	fieldId: string
+) {
+	let docRef: DocumentReference = db.collection("list").doc(listId);
+	let hash = createHash("md5").update(JSON.stringify(jsonArray)).digest("hex");
+	console.dir(`fetched list document with hash: ${hash}`);
+	let doc = await docRef.get();
+	if (doc.exists && doc.data()?.["lastUpdateHash"] === hash) {
+		console.log("hash not changed, skip update");
+		return;
+	}
+	await docRef.set({
+		lastUpdateTime: FieldValue.serverTimestamp(),
+		lastUpdateHash: hash,
+	});
+	await saveDocuments(jsonArray, docRef.collection("item"), fieldId);
+	await saveFields(jsonArray, listId);
 export async function saveList(
 	jsonArray: any[],
 	listId: string,
