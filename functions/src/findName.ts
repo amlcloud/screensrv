dd0cae726a61e4710dd60e76a23d97b2bcb978b9
Oleksii Pavliuk <capetruha@gmail.com>
2023-02-04 00:09:07 +1100

    FindName API

--- functions/src/findName.ts
+++ functions/src/findName.ts
@@ -15,7 +15,7 @@ export const findName = functions.https.onRequest(async (req, res) => {
         return
       }
 
-      // Exequting the query and checking for result 
+      // Exequting the query and checking for result
       let collection = db.collection('index')
       const snapshot = await collection.where('target', '==', name).get()
       if (snapshot.empty) {
@@ -23,9 +23,15 @@ export const findName = functions.https.onRequest(async (req, res) => {
         return;
       }
 
-      //Returning responce with result 
-      res.status(200).send(snapshot.docs.map(async (d) => {
-        let item  = await d.get('ref').get()
-        return item.data()
-    }))
+      //Retrive data from all references and send them in responce
+      Promise.all(snapshot.docs.map(async (d) => {
+
+        let reference = d.get('ref');
+
+        let item = await reference.get();
+        
+        return item.data();
+      })).then(results => {
+        res.status(200).send(results);
+      });
   });
