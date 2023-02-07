545307400ee04fc4e3d1d7dd94302776a8753bbe
Oleksii Pavliuk <capetruha@gmail.com>
2023-02-07 14:01:02 +1100

    20000 limit

--- functions/src/sanction_list_entities.ts
+++ functions/src/sanction_list_entities.ts
@@ -51,33 +51,9 @@ export const GetSanctionsListEntities = functions
 			.collection("list")
 			.doc(list)
 			.collection("item")
-			.limit(19000)
+			.limit(20000)
 			.get();
 		console.log("data is prepared");
-		let len = responce.docs.length;
-		console.log(len);
-
-		if (len > 20000) {
-			responce = await db
-				.collection("list")
-				.doc(list)
-				.collection("item")
-				.limit(20000)
-				.get();
-			try {
-				res
-					.status(200)
-					.send(
-						responce.docs.map((d) =>
-							removeEmptyArrays(removeNullProperties(d.data()))
-						)
-					);
-				return;
-			} catch (err) {
-				res.status(500).send(err);
-				return;
-			}
-		}
 
 		try {
 			res
